aa8c918ffe553941e2727b9c2af5bcbdf4e4c818
unknown <pavanyalala>
2021-07-03 15:54:55 +0530

    UC1 Flip coin is added

--- flipcoinsimulation.sh
+++ flipcoinsimulation.sh
@@ -1,1 +1,20 @@
-" Flip coin simulation problem "
+#!/bin/bash -x
+
+loop=0
+Head=0
+Tail=0
+flip=21
+coin_flip()
+{
+	n=$((RANDOM%2))
+        if [ $n -eq 1 ]
+        then
+                echo "Head Winner."
+                Head=$((Head+1))
+        elif [ $n -eq 0 ]
+        then
+                echo "Tail Winner."
+                Tail=$((Tail+1))
+        fi
+        loop=$((loop+1))
+}
